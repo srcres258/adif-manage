07f60ce7c947d14403b5597eb8fad99f11bf3b64
srcres258 <src.res.211@gmail.com>
2026-04-27 22:41:37 +0800

    adif-manage 0.2.0
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
       {
         packages.default = pythonPkgs.buildPythonApplication {
           pname = "adif-manage";
-          version = "0.1.0";
+          version = "0.2.0";
           inherit src;
 
           pyproject = true;
